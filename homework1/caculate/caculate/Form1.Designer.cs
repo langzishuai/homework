0ab7788cee537c2b5617c4a027ccfd8e4a056b2c
langzishuai <1336954404@qq.com>
2020-02-20 16:00:15 +0800

    调整文件目录

---
+++ homework1/caculate/caculate/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace caculate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.Button();
+            this.num3 = new System.Windows.Forms.Button();
+            this.num6 = new System.Windows.Forms.Button();
+            this.num5 = new System.Windows.Forms.Button();
+            this.num4 = new System.Windows.Forms.Button();
+            this.num9 = new System.Windows.Forms.Button();
+            this.num8 = new System.Windows.Forms.Button();
+            this.num7 = new System.Windows.Forms.Button();
+            this.devide = new System.Windows.Forms.Button();
+            this.multi = new System.Windows.Forms.Button();
+            this.sub = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.doit = new System.Windows.Forms.Button();
+            this.num0 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(32, 64);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(42, 39);
+            this.num1.TabIndex = 0;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = true;
+            this.num1.Click += new System.EventHandler(this.num1_Click);
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(112, 64);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(42, 39);
+            this.num2.TabIndex = 1;
+            this.num2.Text = "2";
+            this.num2.UseVisualStyleBackColor = true;
+            this.num2.Click += new System.EventHandler(this.num2_Click);
+            // 
+            // num3
+            // 
+            this.num3.Location = new System.Drawing.Point(194, 64);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(42, 39);
+            this.num3.TabIndex = 2;
+            this.num3.Text = "3";
+            this.num3.UseVisualStyleBackColor = true;
+            this.num3.Click += new System.EventHandler(this.num3_Click);
+            // 
+            // num6
+            // 
+            this.num6.Location = new System.Drawing.Point(194, 137);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(42, 39);
+            this.num6.TabIndex = 5;
+            this.num6.Text = "6";
+            this.num6.UseVisualStyleBackColor = true;
+            this.num6.Click += new System.EventHandler(this.num6_Click);
+            // 
+            // num5
+            // 
+            this.num5.Location = new System.Drawing.Point(112, 137);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(42, 39);
+            this.num5.TabIndex = 4;
+            this.num5.Text = "5";
+            this.num5.UseVisualStyleBackColor = true;
+            this.num5.Click += new System.EventHandler(this.num5_Click);
+            // 
+            // num4
+            // 
+            this.num4.Location = new System.Drawing.Point(32, 137);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(42, 39);
+            this.num4.TabIndex = 3;
+            this.num4.Text = "4";
+            this.num4.UseVisualStyleBackColor = true;
+            this.num4.Click += new System.EventHandler(this.num4_Click);
+            // 
+            // num9
+            // 
+            this.num9.Location = new System.Drawing.Point(194, 218);
+            this.num9.Name = "num9";
+            this.num9.Size = new System.Drawing.Size(42, 39);
+            this.num9.TabIndex = 8;
+            this.num9.Text = "9";
+            this.num9.UseVisualStyleBackColor = true;
+            this.num9.Click += new System.EventHandler(this.num9_Click);
+            // 
+            // num8
+            // 
+            this.num8.Location = new System.Drawing.Point(112, 218);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(42, 39);
+            this.num8.TabIndex = 7;
+            this.num8.Text = "8";
+            this.num8.UseVisualStyleBackColor = true;
+            this.num8.Click += new System.EventHandler(this.num8_Click);
+            // 
+            // num7
+            // 
+            this.num7.Location = new System.Drawing.Point(32, 218);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(42, 39);
+            this.num7.TabIndex = 6;
+            this.num7.Text = "7";
+            this.num7.UseVisualStyleBackColor = true;
+            this.num7.Click += new System.EventHandler(this.num7_Click);
+            // 
+            // devide
+            // 
+            this.devide.Location = new System.Drawing.Point(307, 239);
+            this.devide.Name = "devide";
+            this.devide.Size = new System.Drawing.Size(42, 39);
+            this.devide.TabIndex = 9;
+            this.devide.Text = "/";
+            this.devide.UseVisualStyleBackColor = true;
+            this.devide.Click += new System.EventHandler(this.devide_Click);
+            // 
+            // multi
+            // 
+            this.multi.Location = new System.Drawing.Point(307, 177);
+            this.multi.Name = "multi";
+            this.multi.Size = new System.Drawing.Size(42, 39);
+            this.multi.TabIndex = 10;
+            this.multi.Text = "*";
+            this.multi.UseVisualStyleBackColor = true;
+            this.multi.Click += new System.EventHandler(this.multi_Click);
+            // 
+            // sub
+            // 
+            this.sub.Location = new System.Drawing.Point(307, 116);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(42, 39);
+            this.sub.TabIndex = 11;
+            this.sub.Text = "-";
+            this.sub.UseVisualStyleBackColor = true;
+            this.sub.Click += new System.EventHandler(this.sub_Click);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(307, 48);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(42, 39);
+            this.add.TabIndex = 12;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 328);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "计算结果";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(141, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "？？？";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 25);
+            this.textBox1.TabIndex = 15;
+            // 
+            // doit
+            // 
+            this.doit.Location = new System.Drawing.Point(291, 293);
+            this.doit.Name = "doit";
+            this.doit.Size = new System.Drawing.Size(71, 50);
+            this.doit.TabIndex = 16;
+            this.doit.Text = "计算";
+            this.doit.UseVisualStyleBackColor = true;
+            this.doit.Click += new System.EventHandler(this.doit_Click);
+            // 
+            // num0
+            // 
+            this.num0.Location = new System.Drawing.Point(112, 276);
+            this.num0.Name = "num0";
+            this.num0.Size = new System.Drawing.Size(42, 39);
+            this.num0.TabIndex = 17;
+            this.num0.Text = "0";
+            this.num0.UseVisualStyleBackColor = true;
+            this.num0.Click += new System.EventHandler(this.num0_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 355);
+            this.Controls.Add(this.num0);
+            this.Controls.Add(this.doit);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.multi);
+            this.Controls.Add(this.devide);
+            this.Controls.Add(this.num9);
+            this.Controls.Add(this.num8);
+            this.Controls.Add(this.num7);
+            this.Controls.Add(this.num6);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button num1;
+        private System.Windows.Forms.Button num2;
+        private System.Windows.Forms.Button num3;
+        private System.Windows.Forms.Button num6;
+        private System.Windows.Forms.Button num5;
+        private System.Windows.Forms.Button num4;
+        private System.Windows.Forms.Button num9;
+        private System.Windows.Forms.Button num8;
+        private System.Windows.Forms.Button num7;
+        private System.Windows.Forms.Button devide;
+        private System.Windows.Forms.Button multi;
+        private System.Windows.Forms.Button sub;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button doit;
+        private System.Windows.Forms.Button num0;
+    }
+}
+
---
+++ homework1/caculate/caculate/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace caculate
+{
+    public partial class Form1 : Form
+    {
+        private string symble = "";
+        private double number1 = double.NaN;
+        private double number2 = double.NaN;
+        private bool isfirst = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void num1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+        }
+
+        private void num2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void num3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void num4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void num5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void num6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void num7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void num8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void num9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            symble = "+";
+            //掉处理函数
+            dealNum();
+        }
+
+        private void sub_Click(object sender, EventArgs e)
+        {
+            symble = "-";
+            dealNum();
+        }
+
+        private void multi_Click(object sender, EventArgs e)
+        {
+            symble = "*";
+            dealNum();
+        }
+
+        private void devide_Click(object sender, EventArgs e)
+        {
+            symble = "/";
+            dealNum();
+        }
+
+        private void dealNum()
+        { 
+            double temp = double.NaN;
+            if(double.TryParse(textBox1.Text,out temp))
+            {
+                if (isfirst)
+                {
+                    number1 = temp;
+                    isfirst = false;
+                }
+                else
+                    number2 = temp;
+
+            }
+            else
+            {
+                MessageBox.Show("输入不正确");
+            }
+
+            textBox1.Text = "";
+        }
+
+        private void doit_Click(object sender, EventArgs e)
+        {
+            if(!isfirst)
+            {
+                dealNum();
+                try
+                {
+                    switch(symble)
+                    {
+                        case "+":
+                            label2.Text = $"{number1 + number2}";
+                            break;
+                        case "-":
+                            label2.Text = $"{number1 - number2}";
+                            break;
+                        case "*":
+                            label2.Text = $"{number1 * number2}";
+                            break;
+                        case "/":
+                            if (number2 != 0)
+                                label2.Text = $"{number1 / number2}";
+                            else
+                                MessageBox.Show("除数不能为0");
+                            break;
+                    }
+                    number1 = number2=double.NaN;
+                    isfirst = true;
+                    symble = "";
+                }catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private void num0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+    }
+}
