b6adf5c618ad169c957262f45a5a9bafcb02545f
langzishuai <1336954404@qq.com>
2020-03-15 19:57:35 +0800

    第四次课堂练习提交

---
+++ homework4/practice_3_6_1/practice_3_6_1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_3_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建测试表
+            GenericList<int> numList = new GenericList<int>();
+            for (int x = 1; x <= 20; x++)
+            {
+                numList.Add(x);
+            }
+            
+            //打印元素
+            Console.WriteLine("打印元素：");
+            numList.Foreach(x => Console.WriteLine(x));
+            //求最大值
+            Console.WriteLine("求最大值：");
+            int max = numList.Head.Data;
+            numList.Foreach(x => { if (x > max) max = x; });
+            Console.WriteLine($"最大值为：{max}");
+            //求最小值
+            Console.WriteLine("求最小值: ");
+            int min = numList.Head.Data;
+            numList.Foreach(x => { if (x < min) min = x; });
+            Console.WriteLine($"最小值为：{min}");
+            //求和
+            Console.WriteLine("求和： ");
+            int sum = 0;
+            numList.Foreach(x => sum += x);
+            Console.WriteLine($"和为：{sum}");
+        }
+    }
+
+    class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T data)
+        {
+            Next = null;
+            Data = data;
+        }
+
+    }
+
+    class GenericList<T>
+    {
+        private Node<T> tail;
+        private Node<T> head;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get { return head; }
+        }
+
+        public void Add(T d)
+        {
+            Node<T> n = new Node<T>(d);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void Foreach(Action<T> action)
+        {
+            Node<T> temp = head;
+            while (temp.Next != null)
+            {
+                action(temp.Data);
+                temp = temp.Next;
+            }
+            action(temp.Data);
+        }
+    }
+        
+
+
+
+    
+}
---
+++ homework4/practice_3_6_2/practice_3_6_2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace practice_3_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock myClock = new Clock();
+            myClock.Alarm += Alarming;
+            myClock.Tick += TimeGo;
+            myClock.setAlarmTime(17, 22, 30);
+
+        }
+
+        static void TimeGo(object sender,TimeEventArgs args)
+        {
+            Console.WriteLine("滴答...");
+        }
+        
+        static void Alarming(object sender,TimeEventArgs args)
+        {
+            Console.WriteLine("闹钟响啦。。叮叮叮叮叮叮叮叮叮");
+        }
+
+    }
+
+    public delegate void TickHandler(object sender, TimeEventArgs args);
+    public delegate void AlarmHandler(object sender, TimeEventArgs args);
+
+    public class TimeEventArgs
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+
+    }
+
+    public class Clock
+    {
+        public event TickHandler Tick;
+        public event AlarmHandler Alarm;
+
+        public TimeEventArgs timeNow=new TimeEventArgs();
+
+        public void setAlarmTime(int hour,int minute,int second)
+        {
+            while(true)
+            {
+                timeNow.Hour = DateTime.Now.Hour;
+                timeNow.Minute = DateTime.Now.Minute;
+                timeNow.Second = DateTime.Now.Second;
+                Console.WriteLine($"当前时间: {timeNow.Hour}:{timeNow.Minute}:{timeNow.Second}");
+                Tick(this, timeNow);
+                if (timeNow.Hour == hour && timeNow.Minute == minute && timeNow.Second == second)
+                    Alarm(this, timeNow);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
